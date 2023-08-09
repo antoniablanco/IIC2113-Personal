b2d43579f1bebbfa679b7c37ad9fb0669dc50ec8
ANTONIA BLANCO <antonia.blanco@uc.cl>
2023-08-09 17:15:00 -0400

    Se agrega el supuesto que los pasajeros que llega a una estación solo quieren ir a una del mismo color

--- Actividad 01/simulador.py
+++ Actividad 01/simulador.py
@@ -8,15 +8,31 @@ M = [[0, 1, [], True, 2], [16, 1, [], False, 3], [24,1,[],False, 2], [32, 1, [],
 # Se cambia para que cuente todas las estaciones
 E = {i: [] for i in range(len(L)) if (L[i] != 0) } # Son las estaciones, calculadas al azar de todas las posiciones
 T = []
-for t in range(1, 961): # Se cambio de 601, es la cantidad de tiempo en minutos
+# Se cambio para que sean 16 horas enves de 10
+for t in range(1, 961): # Es la cantidad de tiempo en minutos
 	for e in E: # Vamos viendo estacion a estación, se hace al azar todas las veces
-		for _ in range(random.randint(0,20)): # Se cambio, Cantidad de personas al azar que estan esperando
+		#print("e es:", e)
+		#print("Color de la estacion es", L[e])
+		# Se cambio para que sea entre 0 y 20
+		for _ in range(random.randint(0,20)): #Cantidad de personas al azar que estan esperando
 			d = random.choice(list(E.keys())) # Se escoge al azar una estación para que sea a la que tiene que llegar el pasajero
-			while d == e: # Si se esta en la misma estación se cambia
-				d = random.choice(list(E.keys())) 
+			recalcular = True
+			while recalcular==True: # Si se esta en la misma estación se cambia
+				if d != e:
+					if L[d]==1 or L[d]==L[e]:
+						recalcular = False
+					else:
+						#print("se vuelve a calcular por que el color es ", L[d])
+						d = random.choice(list(E.keys())) 
+						recalcular = True
+				else:
+					#print("se vuelve a calcular por que el color es ", L[d])
+					d = random.choice(list(E.keys())) 
+					recalcular = True
+			#print("Estacion final es:", d, "y su color:", L[d])
 			E[e].append((d,t)) # Se agrega la persona a la lista de la estación con el tiempo en 
 	for m in M: # Vamos viendo metro a metro
-		print(f"Tren en la posicion {m[0]} es de color {m[4]}")
+		#print(f"Tren en la posicion {m[0]} es de color {m[4]}")
 		# Se cambia por que ahora el true y false no esta en la ultima posición
 		if m[-2]: # Primero se ve si es True o False, que implica si debe parar en la estación pasajeros o no 
 			for i in range(len(m[2])-1, -1, -1): # Se va viendo cada pasajero del metro para bajarse
@@ -39,7 +55,6 @@ for t in range(1, 961): # Se cambio de 601, es la cantidad de tiempo en minutos
 						i += 1
 				else:
 					i += 1
-			print("SE VOLVIO False")
 			m[-2] = False # Necesariamente es False por que no hay 2 estaciones seguidas
 		else:
 			m[0] += m[1] # Se mueve uno de posicion
