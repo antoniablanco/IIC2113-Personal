b4addb91747007ea4e3b405e73605600885459ad
ANTONIA BLANCO <antonia.blanco@uc.cl>
2023-09-02 15:31:45 -0400

    fix: Todos los efectos deriban de la clase abstracta

--- Actividad Photoshop/MyPhotoshop/Effects/ReflejarEffect.cs
+++ Actividad Photoshop/MyPhotoshop/Effects/ReflejarEffect.cs
@@ -3,16 +3,11 @@ using SixLabors.ImageSharp.PixelFormats;
 
 namespace MyPhotoshop.Effects;
 
-public class ReflejarEffect:IPhotoEffect
+public class ReflejarEffect: AbstractEffect
 {
-    private readonly string _description = "Cambia la foto para reflejarla con respecto al eje y.";
-    
-    public string Description
-    {
-        get { return _description; }
-    }
+    public override string Description => "Cambia la foto para reflejarla con respecto al eje y.";
 
-    public Image<Rgb24> Apply(Image<Rgb24> originalImage)
+    public override Image<Rgb24> Apply(Image<Rgb24> originalImage)
     {
         int width = originalImage.Width;
         int height = originalImage.Height;
@@ -21,16 +16,10 @@ public class ReflejarEffect:IPhotoEffect
         {
             for (int y = 0; y < height; y++)
             {
-                int r = originalImage[x, y].R;
-                int g = originalImage[x, y].G;
-                int b = originalImage[x, y].B;
-                byte rByte = (byte)r;
-                byte gByte = (byte)g;
-                byte bByte = (byte)b;
-                reflejarImage[width-x-1, y] = new Rgb24(rByte,gByte,bByte);
-            }   
+                Rgb24 originalColor = originalImage[x, y];
+                reflejarImage[width - x - 1, y] = originalColor;
+            }  
         }
-
         return reflejarImage;
     }
 }
--- Actividad Photoshop/MyPhotoshop/Effects/RotarEffect.cs
+++ Actividad Photoshop/MyPhotoshop/Effects/RotarEffect.cs
@@ -3,16 +3,11 @@ using SixLabors.ImageSharp.PixelFormats;
 
 namespace MyPhotoshop.Effects;
 
-public class RotarEffect:IPhotoEffect
+public class RotarEffect: AbstractEffect
 {
-    private readonly string _description = "Cambia la foto para rotarla en 90°.";
+    public override string Description => "Cambia la foto para rotarla en 90°.";
     
-    public string Description
-    {
-        get { return _description; }
-    }
-
-    public Image<Rgb24> Apply(Image<Rgb24> originalImage)
+    public override Image<Rgb24> Apply(Image<Rgb24> originalImage)
     {
         int width = originalImage.Width;
         int height = originalImage.Height;
@@ -21,13 +16,8 @@ public class RotarEffect:IPhotoEffect
         {
             for (int y = 0; y < height; y++)
             {
-                int r = originalImage[x, y].R;
-                int g = originalImage[x, y].G;
-                int b = originalImage[x, y].B;
-                byte rByte = (byte)r;
-                byte gByte = (byte)g;
-                byte bByte = (byte)b;
-                rotarImage[width-y-1,x] = new Rgb24(rByte,gByte,bByte);
+                Rgb24 originalColor = originalImage[x, y];
+                rotarImage[width-y-1,x] = originalColor;
             }   
         }
 
