34aa3eef66ca854e2766390de0c0ac03730378f1
ANTONIA BLANCO <antonia.blanco@uc.cl>
2023-09-08 11:15:39 -0300

    fix: Se limpia funcion para verificar si el mazo es valido

--- Entrega 2/RawDeal/RawDeal/ValidarDeck.cs
+++ Entrega 2/RawDeal/RawDeal/ValidarDeck.cs
@@ -7,72 +7,99 @@ public class ValidarDeck
         return (mazo.cartasArsenal.Count() == 60);
     }
     
-    public bool TieneSuperStar(Mazo mazo)
+    public bool CumpleTenerSuperStar(Mazo mazo)
     {
         return (mazo.superestar != null);
     }
 
     public bool CumpleSubtipos(Mazo mazo)
     {
-        Dictionary<string, int> dictCount = new Dictionary<string, int>();
-        bool isHeel = false;
-        bool isFace = false;
+        Dictionary<string, int> dictNumeroPorCartas = new Dictionary<string, int>();
+        bool isHeel = MazoContieneIsHeel(mazo);
+        bool isFace = MazoContieneIsFace(mazo);
+        bool cumpleUnique = MazoCumpleCantidadUnique(mazo);
+        bool cumpleNoSetUp = MazoCumpleCantidadNoSetUp(mazo);
+
+        return (!(isHeel && isFace) && cumpleUnique && cumpleNoSetUp);
+    }
+
+    public bool MazoCumpleCantidadUnique(Mazo mazo)
+    {
+        Dictionary<string, int> dictNumeroPorCartas = new Dictionary<string, int>();
         
         foreach (var carta in mazo.cartasArsenal)
         {
-            if (dictCount.ContainsKey(carta.Title))
-            {
-                dictCount[carta.Title]++;
-                if (carta.ContieneSubtipoUnique() && dictCount[carta.Title] > 1)
-                {   
-                    return false;
-                }
+            dictNumeroPorCartas.TryGetValue(carta.Title, out int count);
+            dictNumeroPorCartas[carta.Title] = count + 1;
 
-                if (!carta.ContieneSubtipoSetUp() && dictCount[carta.Title] > 3)
-                {   
-                    return false;
-                }
-            }
-            else
-                dictCount[carta.Title] = 1;
-
-            if (carta.ContieneSubtipoHeel())
-            {
-                isHeel = true;
-            }
-            else if (carta.ContieneSubtipoFace())
-            {
-                isFace = true;
-            }
-
-            if (isHeel && isFace)
-            {
+            if (carta.ContieneSubtipoUnique() && count > 0)
                 return false;
-            }
         }
 
         return true;
     }
     
-    public bool ContieneLogoSuperStar(Mazo mazo)
-    {   
-        List<String> nameSuperStars = new List<string> {"StoneCold", "Undertaker","Mankind", "HHH","TheRock","Kane","Jericho"};
+    public bool MazoCumpleCantidadNoSetUp(Mazo mazo)
+    {
+        Dictionary<string, int> dictNumeroPorCartas = new Dictionary<string, int>();
+        
         foreach (var carta in mazo.cartasArsenal)
-        {   
-            foreach (var logo in nameSuperStars)
-            {   
-                if (carta.ContieneLogoSuperStar(logo) && mazo.superestar.Logo != logo)
-                {   
-                    return false;
-                }
-            }
+        {
+            dictNumeroPorCartas.TryGetValue(carta.Title, out int count);
+            dictNumeroPorCartas[carta.Title] = count + 1;
+
+            if (!carta.ContieneSubtipoSetUp() && count > 2)
+                return false;
         }
 
+        return true;
+    }
+
+    public bool MazoContieneIsHeel(Mazo mazo)
+    {
+        foreach (var carta in mazo.cartasArsenal)
+        {
+            if (carta.ContieneSubtipoHeel())
+                return true;
+        }
+
+        return false;
+    }
+    
+    public bool MazoContieneIsFace(Mazo mazo)
+    {
+        foreach (var carta in mazo.cartasArsenal)
+        {
+            if (carta.ContieneSubtipoFace())
+                return true;
+        }
+
+        return false;
+    }
+    
+    public bool MazoCumpleLogoSuperStar(Mazo mazo)
+    {   
+        foreach (var carta in mazo.cartasArsenal)
+        {   
+            if (!EstaCartaCumpleLogoSuperStar(carta, mazo.superestar.Logo))
+                return false;
+        }
+        return true;
+    }
+
+    public bool EstaCartaCumpleLogoSuperStar(Carta carta, string logoSuperStar)
+    {   
+        List<String> logoSuperStars = new List<string> {"StoneCold", "Undertaker","Mankind", "HHH","TheRock","Kane","Jericho"};
+        foreach (var logo in logoSuperStars)
+        {   
+            if (carta.ContieneLogoSuperStar(logo) && logoSuperStar != logo)
+                return false;
+        }
         return true;
     }
     
     public bool EsValidoMazo(Mazo mazo)
     {
-        return (CumpleTamanoMazo(mazo) && TieneSuperStar(mazo) && CumpleSubtipos(mazo) && ContieneLogoSuperStar(mazo));
+        return (CumpleTamanoMazo(mazo) && CumpleTenerSuperStar(mazo) && CumpleSubtipos(mazo) && MazoCumpleLogoSuperStar(mazo));
     }
 }
