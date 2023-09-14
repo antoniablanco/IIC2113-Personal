2b199bb4f87eb7df6e8d3985b677172ec7b7dcf4
ANTONIA BLANCO <antonia.blanco@uc.cl>
2023-09-14 17:05:18 -0300

    fix: Se sacan las funciones dentro de la clase abstracta

--- Entrega 2/RawDeal/RawDeal/SuperStarClases/Undertaker.cs
+++ Entrega 2/RawDeal/RawDeal/SuperStarClases/Undertaker.cs
@@ -18,32 +18,6 @@ public class Undertaker: SuperStar
         AgregandoCartaHandDelRingSide(jugadorActual);
     }
     
-    public void DescartandoCartasDeHandAlRingSide(Player jugadorActual)
-    {
-        for (int i = 0; i < 2; i++)
-        {   
-            List<string> handFormatoString = visualisarCartas.CrearListaStringCarta(jugadorActual.cartasHand);
-            int cartaSeleccionada =_view.AskPlayerToSelectACardToDiscard(handFormatoString, Name, Name, 2);
-            
-            if (cartaSeleccionada != -1)
-            {
-                Carta cartaDescartada = jugadorActual.cartasHand[cartaSeleccionada];
-                jugadorActual.TraspasoDeCartasEscogiendoCualSeQuiereCambiar(cartaDescartada, jugadorActual.cartasHand, jugadorActual.cartasRingSide);
-            }
-        }
-    }
-    
-    public void AgregandoCartaHandDelRingSide(Player jugadorActual)
-    {
-        List<string> ringSideFormatoString = visualisarCartas.CrearListaStringCarta(jugadorActual.cartasRingSide);
-        int cartaSeleccionada =_view.AskPlayerToSelectCardsToPutInHisHand(Name, 1, ringSideFormatoString);
-        
-        if (cartaSeleccionada != -1)
-        {
-            Carta cartaAgregada = jugadorActual.cartasRingSide[cartaSeleccionada];
-            jugadorActual.TraspasoDeCartasEscogiendoCualSeQuiereCambiar(cartaAgregada, jugadorActual.cartasRingSide, jugadorActual.cartasHand);
-        }
-    }
 
     public override bool PuedeUtilizarSuperAbility(Player jugadorCotrario, Player jugadorActual)
     {
